ea02b5728bb783af3edc9e3fe91bdad5e3b314fc
rohitAmdahl <rohit_designer@outlook.com>
2024-09-19 00:43:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OfficeLayoutProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
