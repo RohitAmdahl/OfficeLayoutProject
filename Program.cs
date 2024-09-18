b6fa2f5adf0041e967c8aa1b2e9436ff070ffdb4
rohitAmdahl <rohit_designer@outlook.com>
2024-09-19 00:46:30 +0200

    Console.Writeline modified

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, its a Office layout project!");
         }
     }
 }
