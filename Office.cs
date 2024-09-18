dab8d86574bedbbec9dced191f7b3145f27b0f1b
rohitAmdahl <rohit_designer@outlook.com>
2024-09-19 01:04:53 +0200

    made office class and enums

---
+++ Office.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeLayoutProject
+{
+    public abstract class Office
+    { 
+        // properties 
+        public int Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+
+        // constructor 
+
+        public Office(int address, string city, string region)
+        { 
+            Address = address;
+            City = city;
+            Region = region;
+        }
+
+    }
+
+
+}
+   
---
+++ RoomsTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeLayoutProject
+{
+    public enum RoomsTypes
+    {
+        ConferenceRoom, EmployOffice, BoosOffice, KitchenRoom, WashRoomLadies, WashRoomGents
+    }
+}
+ 
